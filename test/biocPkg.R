182129a42681e541c885b0b9483b456bd0b2469f
Gahoolee <gahoolee@GahooleedeMacBook-Pro.local>
2015-06-23 23:56:26 +0800

    get bioc all package info

--- test/biocPkg.R
+++ test/biocPkg.R
@@ -1,20 +1,38 @@
 library(XML)
 biocURL<-'http://bioconductor.org/packages/3.2/bioc/'
 biocPackets <- readHTMLTable(biocURL)[[1]]
-head(biocPackets)
+#head(biocPackets)
 
-pkgURL<-'http://bioconductor.org/packages/3.2/bioc/html/a4.html'
-pkg <- readHTMLTable(pkgURL)
-sapply(pkg, class)
+#pkgURL<-'http://bioconductor.org/packages/3.2/bioc/html/a4.html'
+#pkg <- readHTMLTable(pkgURL)
+#sapply(pkg, class)
 
-Maintainer<-unlist(strsplit(as.character(biocPackets$Maintainer),','))
-sort(table(Maintainer))
+#Maintainer<-unlist(strsplit(as.character(biocPackets$Maintainer),','))
+#sort(table(Maintainer))
 
-pkgs<-lapply(biocPackets$Package[1:3], function(pkg){
+pkgs<-lapply(biocPackets$Package[1:2], function(pkg){
   message(pkg)
   pkgURL<-sprintf("http://bioconductor.org/packages/3.2/bioc/html/%s.html", pkg)
-  pkg<-readHTMLTable(pkgURL)
-  pkg[[3]]
+  df<-readHTMLTable(pkgURL, header=F, stringsAsFactors = FALSE, which=3)
+  df$Package<-as.character(pkg)
+  df
 })
 
-save(pkgs, file='pkgs.RData')
+pkgs_df<-do.call(rbind, pkgs)
+pkgs_df$V2[pkgs_df$V2 == ""]<-NA
+
+pkgs_df<-dcast(pkgs_df, Package ~ V1, value.var = 'V2')
+
+pkgs_list<-dlply(pkgs_df, .(Package), function(df){
+  df_list<-as.list(df)
+  df_list<-df_list[!is.na(df_list)]
+  lapply(df_list, function(v){
+    if(length(grep(",", v)) > 0){
+      unlist(strsplit(gsub(" ", "", v), split=','))
+    }else{
+      v
+    }
+  })
+})
+
+save(biocPackets, pkgs, pkgs_df, pkgs_list, file='pkgs.RData')
