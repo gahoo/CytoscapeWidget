58995969929b2b5be1ed06f17aa85424e41a1c7c
Gahoolee <gahoolee@GahooleedeMacBook-Pro.local>
2015-06-23 21:40:15 +0800

    get all pkgs

--- test/biocPkg.R
+++ test/biocPkg.R
@@ -9,3 +9,12 @@ sapply(pkg, class)
 
 Maintainer<-unlist(strsplit(as.character(biocPackets$Maintainer),','))
 sort(table(Maintainer))
+
+pkgs<-lapply(biocPackets$Package[1:3], function(pkg){
+  message(pkg)
+  pkgURL<-sprintf("http://bioconductor.org/packages/3.2/bioc/html/%s.html", pkg)
+  pkg<-readHTMLTable(pkgURL)
+  pkg[[3]]
+})
+
+save(pkgs, file='pkgs.RData')
